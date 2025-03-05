eec2247606acfd150414ea3d7f1aa038e16307e6
Carlos Chávez <tu-email cerobitdata@gmail.com>
2025-03-04 21:50:22 -0300

    challenge amigo secreto

---
+++ app.js
@@ -0,0 +1,101 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
+//  Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+// funcion agregar amigos
+function agregarAmigos() {
+  const nombreAmigo = document.getElementById("amigo").value;
+  console.log("nombres=" + nombreAmigo);
+  console.log("---------arreglo------ "+amigos); 
+  
+  limpiarResultado();
+  limpiarEntrada();
+  
+  if (nombreAmigo !== "") {
+    amigos.push(nombreAmigo);
+    
+    actualizaListaAmigos();
+  } else {
+    alert("Por favor, inserte el nombre");
+    document.getElementById("amigo").value = "";
+  }
+
+  //limpiarEntrada();
+document.getElementById("amigo").focus();
+  return;
+}
+
+//funcion nombres en la interfaz 	
+function actualizaListaAmigos() {	
+    let listaUl = "<ul>"; 	
+	
+    amigos.forEach((nombre) => {	
+        listaUl += `<li>${nombre}</li>`; 	
+    });	
+	
+    listaUl += "</ul>"; 	
+	
+    agregarElementAmigos("#listaAmigos", listaUl);	
+}	
+
+//funcion sortear amigo 
+function sortearAmigo() {
+    limpiarEntrada();
+    document.getElementById("amigo").focus();
+    
+    if ((amigos.length >=1) && (amigos.length< 3)){
+        alert ("Debe ingresar al menos tres Nombres");
+        return;
+    }
+    if (amigos.length === 0)
+    {alert ("No has ingresado un nombre")
+    return;
+    }
+    
+    let nombreAlAzar = Math.floor(Math.random()*amigos.length);
+    let nombreSeleccionado = amigos[nombreAlAzar];
+
+    //agregarElemento ("#resultado", `¡tu amigo secreto es ${nombreSeleccionado}!`);
+    agregarElemento2("#resultado",nombreSeleccionado);
+    
+    //console.log ("nombre seleccionado =" +nombreSeleccionado );
+    
+   // amigos = [];
+    actualizaListaAmigos ();
+}
+
+// funcione de texto
+/*function agregarElemento (elementoResultado, texto){
+    let elementoHTML = document.querySelector (elementoResultado);
+    elementoHTML.innerHTML = texto
+}
+*/
+function agregarElementAmigos(elementoListaAmigos, nombre) {
+  let elementoHTML = document.querySelector(elementoListaAmigos);
+  //elementoHTML.innerHTML = "¡tu amigo secreto es! " +nombre;
+  elementoHTML.innerHTML =  nombre;
+}
+
+function agregarElemento2(elementoResultado, nombreSorteado) {
+  let elementoHTML = document.querySelector(elementoResultado);
+  //elementoHTML.innerHTML = "¡tu amigo secreto es! " +nombre;
+  elementoHTML.innerHTML = "¡Tu amigo secreto es! " +nombreSorteado;
+  amigos=[];
+}
+
+
+//**** */
+// funcion de limpiado de caja 
+function limpiarEntrada () {
+    let entrada =document.querySelector("#amigo");
+    entrada.value= "";
+}
+
+function limpiarResultado() {
+  let resultado = document.querySelector("#resultado");
+  //resultado.value = "";
+resultado.innerHTML="";
+}
+
+
